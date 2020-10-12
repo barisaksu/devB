40f9f4dc3b654c95a1a0004ed5ae53b5eadfd8be
Barış <baris@barisaksu.com>
2020-10-13 01:56:17 +0300

    added path prefix for ci/cd

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome",
+  pathPrefix: "devb",
+  plugins: [],
+};
