28cad6a3bf763428a1364c6e26f229c6658d682b
Barış <baris@barisaksu.com>
2020-10-14 06:02:00 +0300

    add: Header

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,24 @@
 <template>
   <Layout>
+    <Header slot="about" />
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque
+      omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum
+      odit fugit assumenda rem dolores inventore iste reprehenderit maxime!
+      Iusto.
+    </p>
   </Layout>
 </template>
 
 <script>
+import Header from "~/components/Header.vue";
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: "About us",
+  },
+  components: {
+    Header,
+  },
+};
 </script>
